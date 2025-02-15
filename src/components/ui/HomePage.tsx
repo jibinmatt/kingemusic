b483f8155644be4800e6014dd5ff29faaa2c4c26
jerinzech <jerinzjames@gmail.com>
2025-02-15 19:47:39 +0530

    fix homepage and layout

--- src/components/ui/HomePage.tsx
+++ src/components/ui/HomePage.tsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div className='w-[1140px] bg-black h-screen flex justify-center items-center'>HomePage</div>
+    <div className='w-[1440px] bg-black h-screen flex justify-center items-center'>
+
+      <div className="HomePageminus">
+        {/* <div className="kingetextHomePage text-[1100px] text-white font-bold">
+        KING E
+      </div> */}
+      </div>
+
+
+      <div className="HomePageZ1 flex flex-col items-center justify-center ">
+
+        <div className="heroImage p-20">
+          <img src="./heroimagekinge.png" alt="" className='h-[500px] w-[500px]' />
+        </div>
+
+        <div className="ctaDiscoverButton">
+          <button className="ctaDiscoverButton rounded-full h-[70px] w-[300px] bg-white text-black">discover my work</button>
+        </div>
+
+      </div>
+
+
+
+
+    </div>
   )
 }
 
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -4,24 +4,24 @@ function NavBar() {
   return (
     <div className="navBar w-[1440px] flex flex-col justify-between items-center mx-auto p-5">
       <div className="logo h-[40px] w-[40px] ">
-        <img src="/images/logo.png" alt="Logo" />
+        <img src="./logo.png" alt="Logo" />
       </div>
 
       <div className=" ">
         <ul className="flex flex-row justify-between p-5">
-          <li className="p-5">
+          <li className="p-10">
             <a href="">home</a>
           </li>
-          <li className="p-5">
+          <li className="p-10">
             <a href="">about me</a>
           </li>
-          <li className="p-5">
+          <li className="p-10">
             <a href="">discography</a>
           </li>
-          <li className="p-5">
+          <li className="p-10">
             <a href="">events</a>
           </li>
-          <li className="p-5">
+          <li className="p-10">
             <a href="">gallery</a>
           </li>
         </ul>
