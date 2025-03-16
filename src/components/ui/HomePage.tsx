2154a3ee138adb08b511b8ddb569fc3ade2ecbb6
jerinzech <jerinzjames@gmail.com>
2025-03-16 16:18:35 +0530

    refactor layout and styles for HomePage, NavBar, and TextScroll components; adjust animation speed

--- src/components/ui/HomePage.tsx
+++ src/components/ui/HomePage.tsx
@@ -4,18 +4,18 @@ import TextScroll from './TextScroll'
 
 function HomePage() {
   return (
-    <div className='w-[1440px] bg-black h-screen flex flex-col justify-end items-center'>
+    <div className='w-[1440px] bg-black h-screen flex flex-col justify-end items-center p-5'>
 
-      <div className="HomePageZ1 flex flex-col items-center justify-end ">
+      <div className="HomePage flex flex-col items-center justify-end">
 
         <TextScroll />
 
         <div className="heroImage z-[10] w-full h-full flex justify-end items-center mb-[62px] ">
-          <Image src="/images/heroimagekinge.png" alt="hero image" width={350} height={350} />
+          <Image src="/images/heroimagekinge.png" alt="hero image" width={380} height={380} />
         </div>
 
         <div className="ctaDiscoverButton z-[10] flex flex-col items-center justify-items-end mb-[62px] ">
-          <button className="rounded-full h-[60px] w-[250px] bg-white text-black">
+          <button className="rounded-full h-[60px] w-[250px] bg-white text-black text-lg">
             discover my work
           </button>
         </div>
