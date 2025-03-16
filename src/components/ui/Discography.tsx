ecabf094c2957dd138a63165424d99277afc27f0
jerinzech <jerinzjames@gmail.com>
2025-03-16 22:59:53 +0530

    refactor Discography component layout; add multiple Spotify card elements for track display

--- src/components/ui/Discography.tsx
+++ src/components/ui/Discography.tsx
@@ -2,7 +2,41 @@ import React from 'react'
 
 function Discography() {
   return (
-    <div className='w-[1440px] h-screen flex justify-center items-center border-2 border-red-900'>Discography</div>
+    <div className='w-[1440px] h-screen flex justify-evenly items-center border-2 border-red-900 '>
+      <div className="spotifyCard bg-white h-[284px] w-[255px] flex flex-col justify-end items-start rounded">
+        <div className="bg-orange-300 h-[225px] w-[225px] rounded p-[10px] mx-auto"></div>
+        <div className="trackName text-black text-base p-[10px] px-4">
+          Track Name
+        </div>
+      </div>
+      <div className="spotifyCard bg-white h-[284px] w-[255px] flex flex-col justify-end items-start rounded">
+        <div className="bg-orange-300 h-[225px] w-[225px] rounded p-[10px] mx-auto"></div>
+        <div className="trackName text-black text-base p-[10px] px-4">
+          Track Name
+        </div>
+      </div>
+      <div className="spotifyCard bg-white h-[284px] w-[255px] flex flex-col justify-end items-start rounded">
+        <div className="bg-orange-300 h-[225px] w-[225px] rounded p-[10px] mx-auto"></div>
+        <div className="trackName text-black text-base p-[10px] px-4">
+          Track Name
+        </div>
+      </div>
+      <div className="spotifyCard bg-white h-[284px] w-[255px] flex flex-col justify-end items-start rounded">
+        <div className="bg-orange-300 h-[225px] w-[225px] rounded p-[10px] mx-auto"></div>
+        <div className="trackName text-black text-base p-[10px] px-4">
+          Track Name
+        </div>
+      </div>
+      <div className="spotifyCard bg-white h-[284px] w-[255px] flex flex-col justify-end items-start rounded">
+        <div className="bg-orange-300 h-[225px] w-[225px] rounded p-[10px] mx-auto"></div>
+        <div className="trackName text-black text-base p-[10px] px-4">
+          Track Name
+        </div>
+      </div>
+
+
+
+    </div>
   )
 }
 
